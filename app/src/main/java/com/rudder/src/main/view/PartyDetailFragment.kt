7df51f2d5bfeb6c86c070e431f3d32a1abab9726
lsjpjs1 <lsjpjs1@naver.com>
2022-10-05 16:29:17 +0900

    feat: 파티 신청 버튼 경우에 따라 비활성화 기능 추가

--- app/src/main/java/com/rudder/src/main/view/PartyDetailFragment.kt
+++ app/src/main/java/com/rudder/src/main/view/PartyDetailFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
@@ -55,9 +59,26 @@ class PartyDetailFragment : Fragment() {
                 val partyDate = Date(it.partyTime.time)
                 val format: DateFormat = SimpleDateFormat("MMM dd")
                 binding.partyDate = format.format(partyDate)
+
+                binding.partyApplyB.also { button->
+                    when(it.partyStatus){
+                        "FINAL_APPROVE" -> {
+                            button.text = "Approved"
+                        }
+                        "HOST" -> button.text = "You are the HOST"
+                        "REJECT" -> button.text = "DONE"
+                        "PENDING" -> button.text = "Applied"
+                        else -> {
+                            button.background=ContextCompat.getDrawable(requireContext(),R.drawable.button_style)
+                            button.isEnabled = true
+                        }
+                    }
+                }
             }
 
         })
+
+
 
         return binding.root
     }
