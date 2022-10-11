cfaaef4b8e8d2ab0041f958647e13b08f4b7273b
lsjpjs1 <lsjpjs1@naver.com>
2022-10-11 21:18:16 +0900

    4th merge

--- app/src/main/java/com/rudder/MainActivity.kt
+++ app/src/main/java/com/rudder/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.view.View
 import android.view.WindowInsets
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
